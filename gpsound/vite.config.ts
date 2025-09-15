3ce954fa0773e7a8f6a6e07a4b665bfed28c3107
Cyrus Omar <cyology@gmail.com>
2025-09-15 19:43:51 -0400

    update vite config for automerge

--- gpsound/vite.config.ts
+++ gpsound/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import wasm from 'vite-plugin-wasm'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm()],
   build: {
     sourcemap: true
   }
